7561a8c7e450ea240ee9b10cea3e05dd00ac88c0
marcio.pereira <marcio.pereira@usc.unirede.net>
2024-10-20 16:28:09 -0300

    change folder and files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+import { ProductsController } from './controller/product.controller';
+import { ProductService } from './service/product.service';
+import { Product } from './product/entities/product.entity';
+
+
+import { CategoryController } from './controller/category.controller';
+import { CategoryService } from './service/category.service';
+import { Category } from './category/entities/category.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Product, Category],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Product, Category]),
+  ],
+  controllers: [ProductsController, CategoryController],
+  providers: [ProductService, CategoryService],
 })
 export class AppModule {}
